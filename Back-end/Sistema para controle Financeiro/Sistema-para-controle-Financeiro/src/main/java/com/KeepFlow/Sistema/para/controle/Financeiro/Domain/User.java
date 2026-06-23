8f982966b0c25375ce647d407ed22fb36ab16385
Kaueroch <kaue.alcan@gmail.com>
2026-06-22 22:49:09 -0300

    add:Estruturando a arquitetura de pastas do projeto

---
+++ Back-end/Sistema para controle Financeiro/Sistema-para-controle-Financeiro/src/main/java/com/KeepFlow/Sistema/para/controle/Financeiro/Domain/User.java
@@ -0,0 +1,10 @@
+package com.KeepFlow.Sistema.para.controle.Financeiro.Domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+//escrever as regras de dominio que coloquei obsidian e definir o tipos e campos com hibernat
+}
