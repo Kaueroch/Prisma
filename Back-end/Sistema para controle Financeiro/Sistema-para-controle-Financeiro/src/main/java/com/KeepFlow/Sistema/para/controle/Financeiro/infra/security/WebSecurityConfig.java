a6981e024903b14a9beee8489544314703625f60
Kaueroch <kaue.alcan@gmail.com>
2026-09-03 20:34:11 -0300

    fix:tirando linha que libera endpoint pra criar categorias.

--- Back-end/Sistema para controle Financeiro/Sistema-para-controle-Financeiro/src/main/java/com/KeepFlow/Sistema/para/controle/Financeiro/infra/security/WebSecurityConfig.java
+++ Back-end/Sistema para controle Financeiro/Sistema-para-controle-Financeiro/src/main/java/com/KeepFlow/Sistema/para/controle/Financeiro/infra/security/WebSecurityConfig.java
@@ -29,8 +29,12 @@ public class WebSecurityConfig {
                         // Libera totalmente o acesso ao endpoint de registro e login
                         .requestMatchers("/api/v1/user/registrar").permitAll()
                         .requestMatchers("/api/v1/user/login").permitAll()
+			
+
 			//linha que serve para testes de desenvolvimento
-			.requestMatchers("/api/v1/categoria/criarCategoria").permitAll()
+			//.requestMatchers("/api/v1/categoria/criarCategoria").permitAll()
+			
+
 			// Qualquer outra requisição precisará de autenticação
                         .anyRequest().authenticated()
                 )
